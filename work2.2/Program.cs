dd3e236e4b7828957426466ba738117dd72001ba
HelyaFor <olja.formalnova@gmail.com>
2023-06-22 21:22:58 +0300

    feat: add homework

---
+++ work2.2/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Напишите программу, которая выводит третью цифру заданного числа или 
+сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+Console.WriteLine("Пожалуйста, введите число");
+int number = int.Parse (Console.ReadLine ());
+if (number > 99)
+{
+    Console.WriteLine("Третья цифра числа: "+ number);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет в указанном числе");
+}
+
+
+
+/*Console.Write("Введи число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2){
+  Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+}
+else {
+  Console.WriteLine("-> третьей цифры нет");
+}*/
---
+++ work3.2/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Напишите программу, которая принимает на вход координаты двух точек и 
+находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53    
+*/
+
+Console.WriteLine("Пожалуйста, введите значение координаты x1: ");
+int ax = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Пожалуйста, введите значение координаты x2: ");
+int bx = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Пожалуйста, введите значение координаты y1: ");
+int ay = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Пожалуйста, введите значение координаты y2: ");
+int by = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Пожалуйста, введите значение координаты z1: ");
+int az = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Пожалуйста, введите значение координаты z2: ");
+int bz = int.Parse(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow((bx-ax),2)+ Math.Pow((by-ay),2) + Math.Pow((bz-az),2));
+
+Console.WriteLine($"Расстояние между точками равно: {result}");
