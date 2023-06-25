276b408a9b2f4390f7cb497c76f0b911edb41397
HelyaFor <olja.formalnova@gmail.com>
2023-06-25 10:17:00 +0300

    add new exercise

---
+++ work4.1/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Напишите цикл, который принимает на вход два числа (A и B) и 
+возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+Console.WriteLine("Пожалуйста, введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Пожалуйста, введите степень числа: ");
+int n = int.Parse(Console.ReadLine());
+int a = num;
+
+for (int i = 1; i < n; i++)
+{
+   a = a * num;
+}
+Console.WriteLine($"Степень числа: {a}");
