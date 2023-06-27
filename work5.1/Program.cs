7234af94a929fc1d9df6f1fac1970a3c05f9e7c6
HelyaFor <olja.formalnova@gmail.com>
2023-06-28 00:11:12 +0300

    feat: add new example exercise

---
+++ work5.1/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+
+int[] array = new int[7];
+
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+    Console.WriteLine(array[i] + " ");
+}
+
+int num = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+        {
+         num = array [i]; 
+         Console.WriteLine($"Чётные числа в указанном массиве: {num}");
+        }
+}
+
