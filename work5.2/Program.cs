457b66251d605cb92156f2b4e74e222cda47b77d
HelyaFor <olja.formalnova@gmail.com>
2023-06-29 23:27:19 +0300

    feat: add new example exercise

---
+++ work5.2/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0*/
+
+int[] array = new int[4];
+
+for (int i = 0; i < array.Length; i++)
+{
+  array[i] = new Random().Next(1, 10);
+  Console.WriteLine(array[i] + " ");
+}
+
+int sum = 0;
+
+for (int i = 0; i < array.Length; i++)
+  if (i % 2 != 0)
+  {
+    sum = sum + array[i];
+  }
+Console.WriteLine($"Cумма нечётных элментов: {sum}");
