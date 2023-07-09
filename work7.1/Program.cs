13928d41874e50723d232f397815191e2aacf2c5
HelyaFor <olja.formalnova@gmail.com>
2023-07-09 23:06:48 +0300

    feat: add new example exercise[1]

--- work7.1/Program.cs
+++ work7.1/Program.cs
@@ -5,20 +5,31 @@ m = 3, n = 4.
 1 -3,3 8 -9,9
 8 7,8 -7,1 9
 */
-        
-        Console.WriteLine("Введите количество строк: ");
+       
+        Console.WriteLine("Введите количество строк(m): ");
         int rows = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine("Введите количество столбцов: ");
+        Console.WriteLine("Введите количество столбцов(n): ");
         int columns = Convert.ToInt32(Console.ReadLine());
         double[,] array = new double[rows, columns];
-
-        for (int i = 0; i < array.GetLength(0); i++)
+        Random random = new Random();
         {
-            for (int j = 0; j < array.GetLength(1); j++)
-            {
-                array[i, j] = new Random().NextDouble();
-                Console.Write(array[i, j] + " ");
-            }
-        Console.WriteLine();
+        {  
+            for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        array[i, j] = random.Next(-99, 99)/10.0;
+                    }
+                }
+                Console.Write(" ");
         }
+           for(int i = 0; i < array.GetLength(0); i++)
+            {
+                for(int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+            Console.WriteLine(" ");
+            }
+        }   
