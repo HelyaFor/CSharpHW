017cf7e5fc8449d500383a9ce8e3b3ffd01c12fd
HelyaFor <olja.formalnova@gmail.com>
2023-07-09 23:09:06 +0300

    feat: add new example exercise[3]

--- work7.3/Program.cs
+++ work7.3/Program.cs
@@ -8,32 +8,40 @@
 Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 */
 
-Console.WriteLine("Введите количество строк: ");
-int rows = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Введите количество строк: ");
+        int rows = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите количество столбцов: ");
-int columns = Convert.ToInt32(Console.ReadLine());
-
-int[,] array = new int[4, 4];
-
- for (int i = 0; i < array.GetLength(0); i++)
+        Console.WriteLine("Введите количество столбцов: ");
+        int columns = Convert.ToInt32(Console.ReadLine());
+        Random random = new Random();
+        int[,] array = new int[rows, columns];
         {
-            for (int j = 0; j < array.GetLength(1); j++)
-            {
-                array[i, j] = new Random().Next(1, 5);
-                Console.Write(array[i, j] + " ");
-            }
-        Console.WriteLine();
-        }
- for (int i = 0; i < array.GetLength(1); i++)
-        {
-        double sum = 0;
-        double result = 0;
-            for (int j = 0; j < array.GetLength(0); j++)
+        {  
+            for (int i = 0; i < array.GetLength(0); i++)
                 {
-                    sum += array[j, i];
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        array[i, j] = random.Next(1, 10);
+                    }
                 }
-            result = sum / array.GetLength(0);
-            Console.Write($"Cреднее арифметическое элементов в столбце: {result}; ");
         }
-        
+           for(int i = 0; i < array.GetLength(0); i++)
+            {
+                for(int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+            Console.WriteLine(" ");
+            }
+            for (int i = 0; i < array.GetLength(1); i++)
+                    {
+                    double sum = 0;
+                    double result = 0;
+                        for (int j = 0; j < array.GetLength(0); j++)
+                            {
+                                sum += array[j, i];
+                            }
+                        result = sum / array.GetLength(0);
+                        Console.WriteLine($"Cреднее арифметическое элементов в столбце: {result}; ");
+        }
+        } 
