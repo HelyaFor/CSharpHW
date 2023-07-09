31339bdb5a2100f41d01bad64005b8dbc23aa09b
HelyaFor <olja.formalnova@gmail.com>
2023-07-09 23:05:56 +0300

    feat: add new example exercise[1]

--- work7.2/Program.cs
+++ work7.2/Program.cs
@@ -12,22 +12,38 @@
 
         Console.WriteLine("Введите количество столбцов: ");
         int columns = Convert.ToInt32(Console.ReadLine());
+        Random random = new Random();
         int[,] array = new int[5, 6];
-
-        for (int i = 0; i < array.GetLength(0); i++)
         {
-            for (int j = 0; j < array.GetLength(1); j++)
-            {
-                array[i, j] = new Random().Next(1, 10);
-                Console.Write(array[i, j] + " ");
-            }
-        Console.WriteLine();
+        {  
+            for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        array[i, j] = random.Next(1, 10);
+                    }
+                }
         }
-        if (rows < array.GetLength(0) && columns < array.GetLength(1)) 
+           for(int i = 0; i < array.GetLength(0); i++)
             {
-                Console.WriteLine($"Элемент найден в массиве: {array[rows, columns]}");
+                for(int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+            Console.WriteLine(" ");
             }
-        else 
-            {
-                Console.WriteLine($"Указанный элемент отсутствует в массиве");
-            }
+            if (rows < array.GetLength(0) && columns < array.GetLength(1)) 
+                {
+                    Console.WriteLine($"Элемент найден в массиве: {array[rows, columns]}");
+                }
+            else 
+                {
+                    Console.WriteLine($"Указанный элемент отсутствует в массиве");
+                }
+        }   
+
+        
+        
+        
+
+       
