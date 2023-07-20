c608f1e21c1ba9704d45fa93d891ea50d450ec0a
HelyaFor <olja.formalnova@gmail.com>
2023-07-21 01:13:04 +0300

    feat: add new example exercise[2]

---
+++ work9.2/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задайте значения M и N. Напишите программу, которая найдёт
+ сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+Console.WriteLine("Пожалуйста, введите целое число (M): ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Пожалуйста, введите целое число (N): ");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int amountNum (int m)
+{
+if (m >= 1 && m <= n)
+{
+    sum += m;
+    amountNum(m + 1);
+}
+return sum;
+}
+int result = amountNum(m);
+Console.WriteLine("Сумма натуральных чисел: " + result);
